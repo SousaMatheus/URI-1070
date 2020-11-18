250536efd5305c2e18ed0623cad21f99e25d2a4f
Matheus Sousa <ma.sousa.soares@gmail.com>
2020-11-17 21:59:57 -0300

    refatorado programa

--- Program.cs
+++ Program.cs
@@ -6,42 +6,18 @@ namespace URI_1070
     {
         static void Main(string[] args)
         {
-            int valor = int.Parse(Console.ReadLine());
-            int impar1 = 0;
-            int impar2 = 0;
-            int impar3 = 0;
-            int impar4 = 0;
-            int impar5 = 0;
-            int impar6 = 0;
-
-            if (valor % 2 == 0){
-                impar1 = valor + 1;
-                impar2 = impar1 + 2;
-                impar3 = impar2 + 2;
-                impar4 = impar3 + 2;
-                impar5 = impar4 + 2;
-                impar6 = impar5 + 2;
-                Console.WriteLine(impar1);
-                Console.WriteLine(impar2);
-                Console.WriteLine(impar3);
-                Console.WriteLine(impar4);
-                Console.WriteLine(impar5);
-                Console.WriteLine(impar6);
-            }
-            else {
-                impar1 = valor;
-                impar2 = impar1 + 2;
-                impar3 = impar2 + 2;
-                impar4 = impar3 + 2;
-                impar5 = impar4 + 2;
-                impar6 = impar5 + 2;
-                Console.WriteLine(impar1);
-                Console.WriteLine(impar2);
-                Console.WriteLine(impar3);
-                Console.WriteLine(impar4);
-                Console.WriteLine(impar5);
-                Console.WriteLine(impar6);
-            }               
+            int x = int.Parse(Console.ReadLine());
+            
+            if (x % 2 == 0){
+                x = x + 1;
+            }            
+            
+                Console.WriteLine(x);
+                Console.WriteLine(x + 2);
+                Console.WriteLine(x + 4);
+                Console.WriteLine(x + 6);
+                Console.WriteLine(x + 8);
+                Console.WriteLine(x + 10);                           
             
         }
     }
